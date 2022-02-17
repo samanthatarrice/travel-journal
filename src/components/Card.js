551294bd225cde842ccd3461ebe919f4d810b081
Samantha Tarrice <s.tarrice@gmail.com>
2022-02-16 21:05:09 -0300

    Update data property values.

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,12 +3,12 @@ import location from '../images/location.png'
 function Card(props) {
   return (
     <article className='card'>
-      <img className='location-img' src={require(`../images/${props.image}`)} />
+      <img className='location-img' src={require(`../images/${props.image}`)} alt={props.alt} />
       <div className='card-text'>
         <div className='location-info'>
-          <img className='map-pin' src={location} />
+          <img className='map-pin' src={location} alt='Map pin' />
           <p className='country'>{props.country.toUpperCase()}</p>
-          <a href={props.googleMapsUrl} target='_blank'>View on Google Maps</a>
+          <a href={props.googleMapsUrl} target='_blank' rel='noreferrer'>View on Google Maps</a>
         </div>
         <h1>{props.title}</h1>
         <p className='date'>{props.date}</p>
--- src/data.js
+++ src/data.js
@@ -1,38 +1,52 @@
 export default [
   {
-      id: 1,
-      image: "patagonia.jpeg",
-      country: "Argentina",
-      googleMapsUrl: "https://goo.gl/maps/NnT9CLpX8AvQCxrJ6",
-      title: "Perrito Morreno Glacier",
-      date: "October 2018",
-      description: "Visiting Patagonia in Argentina was an amazing trip. We were about a mile away from hiking all the way up Monte Fitz Roy, the mountains you see on the Patagonia company logo. But, we decided not because the conditions were icey and I was five months pregnant. We did see glaciers up close, and I've never been more freezing in my life. A few times we could see ice melting off, which was exciting, but seeing how far the glaciers had receeded was a real eye opener."
+    id: 1,
+    image: "patagonia.jpeg",
+    alt: "Sam and her husband in front of Perrito Morreno Glacier",
+    country: "Argentina",
+    googleMapsUrl: "https://goo.gl/maps/NnT9CLpX8AvQCxrJ6",
+    title: "Perrito Morreno Glacier",
+    date: "October 2018",
+    description: "Visiting Patagonia in Argentina was an amazing trip. We were about a mile away from hiking all the way up Monte Fitz Roy, the mountains you see on the Patagonia company logo. But, we decided not because the conditions were icey and I was five months pregnant. It was still an incredible hike. We also see glaciers up close, and I've never been more freezing in my life. A few times we could see ice melting off, which was exciting, but seeing how far the glaciers had receeded was a real eye opener."
   },
   {
     id: 2,
-    image: "fuji.png",
+    image: "mexico.jpeg",
+    alt: "Sam standing in front of Teotihuacan.",
+    country: "Mexico",
+    googleMapsUrl: "https://goo.gl/maps/9MvP39phP9f7q1VV7",
+    title: "Teotihuacan",
+    date: "July 2018",
+    description: "Mexico City is one of my favorite cities. There is so much great history, culture, and food, and so much to explore! I could definitely see my family possibly living there one day. This was taken on the ruins of Teotihuacan in a small town outside of CDX."
+  },
+  {
+    id: 3,
+    image: "peru.jpeg",
+    alt: "Sam standing face to face with a llama in Machu Picchu.",
     country: "Peru",
     googleMapsUrl: "https://goo.gl/maps/Tzmdkh94ezEP2ckc7",
     title: "Machu Picchu",
-    date: "July 2018",
-    description: "loremkdfjalkdfjal;sfja;ldsfjalfdj"
+    date: "January 2018",
+    description: "Peru is such an icredible country with so much history, friendly people, and a diverse landscape. My husband and I explored the country, and of course visited the Sacred Valley and Machu Picchu. Here I am with one of the llamas that roam free around the ruins."
   },
   {
-      id: 3,
-      image: "salvador.jpeg",
-      country: "Brazil",
-      googleMapsUrl: "https://goo.gl/maps/X2qvsYs29V3tVVYe9",
-      title: "Boipeba",
-      date: "October 2017",
-      description: "To get to Boipeba you have to first, get to Salvador, Bahia. Then, you take a ferry to a town across the bay. From there you find a taxi to drive you to a town an hour away. Then, you have to wait for a speedboat to drive you an hour down the coast. The trip is more than worth it though. I've never experienced such a remote beach getaway like this!"
+    id: 4,
+    image: "salvador.jpeg",
+    alt: "Sam sitting in a hammock in Boipeba.",
+    country: "Brazil",
+    googleMapsUrl: "https://goo.gl/maps/X2qvsYs29V3tVVYe9",
+    title: "Boipeba",
+    date: "October 2017",
+    description: "To get to Boipeba you have to first, get to Salvador, Bahia. Then, you take a ferry to a town across the bay. From there you find a taxi to drive you to a town an hour away. Then, you have to wait for a speedboat to drive you an hour down the coast. The trip is more than worth it though. I've never experienced such a remote beach getaway like this!"
   },
   {
-      id: 4,
-      image: "paris.jpeg",
-      country: "France",
-      googleMapsUrl: "https://goo.gl/maps/Tzmdkh94ezEP2ckc7",
-      title: "Tour Eiffel",
-      date: "Summer of 2008",
-      description: "This was a solo trip to southern Europe that started my wanderlust. After graduating college, I decided to celebrate by spending a little over a month exploring Italy, Switzerland, France, and Spain."
+    id: 5,
+    image: "paris.jpeg",
+    alt: "Sam standing in front of the Eiffel Tower.",
+    country: "France",
+    googleMapsUrl: "https://goo.gl/maps/Tzmdkh94ezEP2ckc7",
+    title: "Eiffel Tower",
+    date: "Summer of 2008",
+    description: "This was a solo trip to southern Europe that started my wanderlust. After graduating college, I decided to celebrate by spending a little over a month exploring Italy, Switzerland, France, and Spain."
   }
 ]
